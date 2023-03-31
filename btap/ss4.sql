9c75f4754b912aa208abf9d5f46214b60a851740
soljinn <soljin97@gmail.com>
2023-03-31 08:21:19 +0700

    update

--- btap/ss4.sql
+++ btap/ss4.sql
@@ -13,16 +13,18 @@ limit 1;
 
 -- Thông kê các đầu sách chưa được mượn
 select b.title,b.b_id,count(b.b_id) as `amount` 
-from borrows as bo
-join books as b on b.b_id = bo.b_id
-group by b.b_id
-having amount = (
-select count(b.b_id) as amount from borrows as bo
-join books as b on b.b_id = bo.b_id
-group by b.b_id
-order by amount desc limit 0
-);
+from books as b
+join borrows as bo on bo.b_id = b.b_id
+where bo.b_id not in (
+select bo.b_id from borrows as bo
+ where curdate() between bo.borrow_date and bo.return_date)
+group by b.b_id;
 
+select books.name,books.id
+from books
+left join borrows on books.id=borrows.book_id 
+where borrows.book_id not in (select borrows.book_id from borrows where curdate() between borrows.borrow_date and borrows.return_date)
+group by books.id;
 -- Lấy ra danh sách các học viên đã từng mượn sách và sắp xếp  theo số lượng mượn sách từ lớn đến nhỏ
 select s_name, count(bo.b_id) as `amount` 
 from student as s
